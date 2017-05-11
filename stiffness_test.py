1b11ca99c9e00795ce092676fd0b58234dcb3151
blundell1 <i.blundell@fz-juelich.de>
2017-05-11 09:49:54 +0200

    added a multi-comp model and a hh model

--- stiffness_test.py
+++ stiffness_test.py
@@ -51,10 +51,10 @@ class TestStiffnessChecker(unittest.TestCase):
         #check_ode_system_for_stiffness(odes, default_values, threshold_body, reset_statement)
                 
     def test_aeif_cond_alpha(self):
-        threshold_body = "y_0 >= V_peak"
+        threshold_body = "y_0 >= V_th"
         reset_statement = "y_0 = V_peak"
 
-        default_values =["name = 'aeif_cond_alpha'",
+        default_values = ["name = 'aeif_cond_alpha'",
                          "dimension = 6",
                          "tau_synE = 0.2",
                          "tau_synI = 2.0",
@@ -79,11 +79,109 @@ class TestStiffnessChecker(unittest.TestCase):
                 "f_4 = y_3 - (y_4 / tau_synI)",
                 "f_5 = (a * (y_0 - E_L) - y_5) / tau_w"]
 
-        check_ode_system_for_stiffness(odes, default_values, threshold_body, reset_statement)
+        #check_ode_system_for_stiffness(odes, default_values, threshold_body, reset_statement)
+
+    def test_hh_iaf_psc_alpha(self):
+        threshold_body = ""
+        reset_statement = "" 
+    
+        default_values =["g_Na = 12000.0",
+                         "g_K = 3600.0",
+                         "g_L = 30.0",
+                         "C_m = 100.0",
+                         "E_Na = 50.0",
+                         "E_K = -77.0",
+                         "E_L = -54.402",
+                         "tau_synE = 0.2",
+                         "tau_synI = 2.0",
+                         "I_e = 0.0"]
+     
+        odes= ["function V = y_0",
+               "function m = y_1",
+               "function h = y_2",
+               "function n = y_3",
+               "function dI_ex = y_4",
+               "function I_ex = y_5",
+               "function dI_in = y_6",
+               "function I_in = y_7",
+               "function alpha_n =  ( 0.01 * ( V + 55. ) ) / ( 1. - exp( -( V + 55. ) / 10. ) )",
+               "function beta_n = 0.125 * exp( -( V + 65. ) / 80. )",
+               "function alpha_m = ( 0.1 * ( V + 40. ) ) / ( 1. -exp( -( V + 40. ) / 10. ) )",
+               "function beta_m = 4. * exp( -( V + 65. ) / 18. )",
+               "function alpha_h = 0.07 * exp( -( V + 65. ) / 20. )",
+               "function beta_h = 1. / ( 1. + exp( -( V + 35. ) / 10. ) )",
+               "function I_Na = g_Na * m * m * m * h * ( V - E_Na )",
+               "function I_K = g_K * n * n * n * n * ( V - E_K )",
+               "function I_L = g_L * ( V - E_L )",
+               "f_0 = ( -( I_Na + I_K + I_L ) + I_stim + I_e + I_ex + I_in ) / C_m",
+               "f_1 = alpha_m * ( 1 - m ) - beta_m * m",
+               "f_2 = alpha_h * ( 1 - h ) - beta_h * h",
+               "f_3 = alpha_n * ( 1 - n ) - beta_n * n", 
+               "f_4 = -dI_ex / tau_synE",
+               "f_5 = dI_ex - ( I_ex / tau_synE )",
+               "f_6 = -dI_in / tau_synI",
+               "f_7 = dI_in - ( I_in / tau_synI )"]
 
 
         # TODO runs to slow check_ode_system_for_stiffness(odes, default_values, threshold_body, reset_statement)
-        
+
+        def test_with_iaf_cond_alpha_mc(self):
+            threshold_body = "y_0 >= V_th"
+            reset_statement = "y_0 = V_reset"
+
+            default_values = ["neuron_name = 'iaf_cond_alpha_mc'",
+                              "V_th = -55.0",
+                              "V_reset = -60.0",
+                              "E_L_1 = -70.0",
+                              "tau_synE_1 = 0.5",
+                              "tau_synI_1 = 2.0",
+                              "E_ex_1 = 0.0",
+                              "E_in_1 = -85.0",
+                              "gL_1 = 10",
+                              "I_e_1 = 0.0",
+                              "C_m_1 = 150",
+                              "start_values_1 = [E_L_1, 0.0, 0.0, 0.0, 0.0]",
+                              "initial_values_1 = [0, math.e / tau_synE_1, 0, -math.e / tau_synI_1, 0]",
+                              "E_L_2 = -70.0",
+                              "tau_synE_2 = 0.5",
+                              "tau_synI_2 = 2.0",
+                              "E_ex_2 = 0.0",
+                              "E_in_2 = -85.0",
+                              "gL_2 = 5.0",
+                              "I_e_2 = 0.0",
+                              "C_m_2 = 75.",
+                              "start_values_2 = [E_L_2, 0.0, 0.0, 0.0, 0.0]",
+                              "initial_values_2 = [0, math.e / tau_synE_2, 0, -math.e / tau_synI_2, 0]",
+                              "E_L_3 = -70.0",
+                              "tau_synE_3 = 0.5",
+                              "tau_synI_3 = 2.0",
+                              "E_ex_3 = 0.0",
+                              "E_in_3 = -85.0",
+                              "gL_3 = 10",
+                              "I_e_3 = 0.0",
+                              "C_m_3 = 150",
+                              "start_values_3 = [E_L_3, 0.0, 0.0, 0.0, 0.0]",
+                              "initial_values_3 = [0, math.e / tau_synE_3, 0, -math.e / tau_synI_3, 0]",
+                              "conn_1 = 2.5",
+                              "conn_2 = 1.0"]
+
+            odes = ["f_0 = (-(g_L_1 * (y_0 - E_L_1)) - (y_2 * (y_0 - E_ex_1)) - y_4 * (y_0 - E_in_1) + conn_1 * (y_0 - y_5) + I_e_1) / C_m_1",
+                    "f_1 = -y_1 / tau_synE_1",
+                    "f_2 = y_1 - (y_2 / tau_synE_1)",
+                    "f_3 = -y_3 / tau_syn_I_1",
+                    "f_4 = y_3 - (y_4 / tau_synI_1)"
+                    "f_5 = (-(g_L_2 * (y_5 - E_L_2)) - (y_7 * (y_5 - E_ex_2)) - y_9 * (y_5 - E_in_2) + conn_1 * (y_5 - y_0) + conn_2 * (y_5 - y_10) + I_e_2) / C_m_2",
+                    "f_6 = -y_6 / tau_synE_2",
+                    "f_7 = y_6 - (y_7 / tau_synE_2)",
+                    "f_8 = -y_8 / tau_synI_2",
+                    "f_9 = y_8 - (y_9 / tau_synI_2)"
+                    "f_10 = (-(g_L_3 * (y_10 - E_L_3)) - (y_12 * (y_10 - E_ex_3)) - y_14 * (y_10 - E_in_3) + conn_2 * (y_10 -y_5) + I_e_3) / C_m_3",
+                    "f_11 = -y_11 / tau_synE_3",
+                    "f_12 = y_11 - (y_12 / tau_synE_3)",
+                    "f_13= -y_13 / tau_synI_3",
+                    "f_14 = y_13 - (y_14 / tau_synI)"]
+
+        check_ode_system_for_stiffness(odes, default_values, threshold_body, reset_statement)
         
 if __name__ == '__main__':
     unittest.main()
