9195385a80a7540219ae9ecc1da2eabb5f1d5b33
Jochen Martin Eppler <jougs@gmx.net>
2017-07-23 15:18:37 +0200

    Fixing call to undefined function

--- shapes_test.py
+++ shapes_test.py
@@ -9,8 +9,8 @@ class TestThatShapeIsConvertedToODEs(unittest.TestCase):
         shape_inh = ShapeFunction("I_in", "(e/tau_syn_in) * t * exp(-t/tau_syn_in)")
         shape_exc = ShapeFunction("I_ex", "(e/tau_syn_ex) * t * exp(-t/tau_syn_ex)")
 
-        print(shape_inh.get_ode_form())
-        print(shape_exc.get_ode_form())
+        print(shape_inh.nestml_ode_form)
+        print(shape_exc.nestml_ode_form)
 
 if __name__ == '__main__':
     unittest.main()
