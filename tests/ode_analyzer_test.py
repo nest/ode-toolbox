b188f2071bad02594948aa3181ff1a351063c1c6
DimitriPlotnikov <d.plotnikov@fz-juelich.de>
2017-10-19 08:27:02 +0200

    fix naming

--- tests/ode_analyzer_test.py
+++ tests/ode_analyzer_test.py
@@ -58,7 +58,7 @@ class TestSolutionComputation(unittest.TestCase):
         self.assertTrue(len(result["shape_ode_definitions"]) == 2)
         self.assertTrue(len(result["shape_state_variables"]) == 4)
 
-    def test_iaf_cond_alpha(self):
+    def test_iaf_cond_alpha_mixed(self):
         result = ode_analyzer.main(["iaf_cond_alpha_mixed.json"])
         result = json.loads(result)
 
