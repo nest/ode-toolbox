82039b0b6221921e436733c010a4c8be46679806
Jochen Martin Eppler <jougs@gmx.net>
2017-10-27 18:49:19 +0200

    Update and extend the tests to run with the new structure

--- tests/test_ode_analyzer.py
+++ tests/test_ode_analyzer.py
@@ -1,9 +1,38 @@
+#
+# test_ode_analyzer.py
+#
+# This file is part of the NEST ODE toolbox.
+#
+# Copyright (C) 2017 The NEST Initiative
+#
+# The NEST ODE toolbox is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
+#
+# The NEST ODE toolbox is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import json
+import os
 import unittest
 
-import ode_analyzer
-from analytic import Propagator
-from shapes import shape_from_function
+from .context import odetoolbox
+from odetoolbox.analytic import Propagator
+from odetoolbox.shapes import shape_from_function
+
+
+def open_json(fname):
+    absfname = os.path.join(os.path.abspath(os.path.dirname(__file__)), fname)
+    with open(absfname) as infile:
+        indict = json.load(infile)
+    return indict
 
 
 class TestSolutionComputation(unittest.TestCase):
@@ -15,19 +44,19 @@ class TestSolutionComputation(unittest.TestCase):
 
         ode_symbol = "V_m"
         ode_definition = "-V_m/Tau + (I_in + I_ex + I_e) / C_m"
-
-        self.assertEqual(True,
-                         ode_analyzer.ode_is_lin_const_coeff(ode_symbol, ode_definition, shapes))
+        result = odetoolbox.ode_is_lin_const_coeff(ode_symbol, ode_definition, shapes)
+        self.assertTrue(result)
 
         ode_symbol = "V_m"
         ode_definition = "(I_in*V_m)/C_m"
-        self.assertEqual(False,
-                         ode_analyzer.ode_is_lin_const_coeff(ode_symbol, ode_definition, shapes))
+        result = odetoolbox.ode_is_lin_const_coeff(ode_symbol, ode_definition, shapes)
+        self.assertFalse(result)                         
 
         ode_symbol = "V_m"
         ode_definition = "(V_m*V_m)/C_m"
-        self.assertEqual(False,
-                         ode_analyzer.ode_is_lin_const_coeff(ode_symbol, ode_definition, shapes))
+        result = odetoolbox.ode_is_lin_const_coeff(ode_symbol, ode_definition, shapes)
+        self.assertFalse(result)
+
 
     def test_propagator_matrix(self):
         shape_inh = shape_from_function("I_in", "(e/tau_syn_in) * t * exp(-t/tau_syn_in)")
@@ -39,37 +68,41 @@ class TestSolutionComputation(unittest.TestCase):
 
         propagator = Propagator(ode_symbol, ode_definition, shapes)
 
-        self.assertTrue(len(propagator.propagator_matrices) == 2)  # one matrix for every shape
+        self.assertEqual(len(propagator.propagator_matrices), 2)  # one per shape
         self.assertTrue(len(propagator.ode_updates) > 0)
 
+
     def test_iaf_psc_alpha(self):
-        result = ode_analyzer.main(["iaf_psc_alpha.json"])
-        result = json.loads(result)
+        indict = open_json("iaf_psc_alpha.json")
+        result = odetoolbox.analysis(indict)
 
         self.assertEqual("analytical", result["solver"])
         self.assertTrue(len(result["propagator"]) > 0)
+
 
     def test_iaf_psc_alpha_mixed(self):
-        result = ode_analyzer.main(["iaf_psc_alpha_mixed.json"])
-        result = json.loads(result)
+        indict = open_json("iaf_psc_alpha_mixed.json")
+        result = odetoolbox.analysis(indict)
 
         self.assertEqual("analytical", result["solver"])
         self.assertTrue(len(result["propagator"]) > 0)
 
+
     def test_iaf_cond_alpha(self):
-        result = ode_analyzer.main(["iaf_cond_alpha.json"])
-        result = json.loads(result)
-        print json.dumps(result, indent=2)
-        self.assertEqual("numeric", result["solver"])
+        indict = open_json("iaf_cond_alpha.json")
+        result = odetoolbox.analysis(indict)
+
+        self.assertEqual("numeric-explicit", result["solver"])
         self.assertTrue(len(result["shape_initial_values"]) == 4)
         self.assertTrue(len(result["shape_ode_definitions"]) == 2)
         self.assertTrue(len(result["shape_state_variables"]) == 4)
 
-    def test_iaf_cond_alpha_mixed(self):
-        result = ode_analyzer.main(["iaf_cond_alpha_mixed.json"])
-        result = json.loads(result)
 
-        self.assertEqual("numeric", result["solver"])
+    def test_iaf_cond_alpha_mixed(self):
+        indict = open_json("iaf_cond_alpha_mixed.json")
+        result = odetoolbox.analysis(indict)
+
+        self.assertEqual("numeric-explicit", result["solver"])
         self.assertTrue(len(result["shape_initial_values"]) == 4)
         self.assertTrue(len(result["shape_ode_definitions"]) == 2)
         self.assertTrue(len(result["shape_state_variables"]) == 4)
--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -1,6 +1,28 @@
+#
+# test_shapes.py
+#
+# This file is part of the NEST ODE toolbox.
+#
+# Copyright (C) 2017 The NEST Initiative
+#
+# The NEST ODE toolbox is free software: you can redistribute it
+# and/or modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation, either version 2 of
+# the License, or (at your option) any later version.
+#
+# The NEST ODE toolbox is distributed in the hope that it will be
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NEST.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import unittest
 
-import shapes
+from .context import odetoolbox
+from odetoolbox import shapes
 
 
 class TestShapeFunction(unittest.TestCase):
@@ -11,9 +33,6 @@ class TestShapeFunction(unittest.TestCase):
         self.assertIsNotNone(shape_inh.ode_definition)
         self.assertIsNotNone(shape_exc.ode_definition)
 
-        print(shape_inh.ode_definition)
-        print(shape_exc.ode_definition)
-
 
 class TestShapeODE(unittest.TestCase):
 
@@ -22,8 +41,6 @@ class TestShapeODE(unittest.TestCase):
         shape_inh = shapes.shape_from_ode("alpha", "-1/tau**2 * alpha -2/tau * alpha'", ["0", "e/tau"])
         self.assertIsNotNone(shape_inh.derivative_factors)
 
-        print shape_inh.derivative_factors
-        
         
 if __name__ == '__main__':
     unittest.main()
