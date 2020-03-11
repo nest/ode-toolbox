b14d2b0a9c318c5f532efff082dff9efa3c71de2
C.A.P. Linssen <charl@turingbirds.com>
2020-03-11 18:31:19 +0100

    refactor documentation for readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -88,7 +88,7 @@ for fn in matches:
 	if "sphinx-apidoc" in fn:
 		continue
 	fn_from = fn
-	fn_to = os.path.join(static_docs_dir, "sphinx-apidoc", fn[len(static_docs_dir)+1:])
+	fn_to = os.path.join(static_docs_dir, ".", fn[len(static_docs_dir)+1:])
 	print("From " + fn_from + " to " + fn_to)
 	os.system('install -v -D ' + fn_from + " " + fn_to)
 #os.system('for i in `find .. -name "*.rst"` ; do if [[ ${i} != *"sphinx-apidoc"* ]] ; then install -v -D ${i} ${i/\.\.\//}; fi ; done')
@@ -180,7 +180,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['css', 'fig']
 
 # -- Options for HTMLHelp output ------------------------------------------
 
