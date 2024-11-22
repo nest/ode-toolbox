2c3feeb2d61d2e1fcb0c2ce361d24074ba0152b8
C.A.P. Linssen <charl@turingbirds.com>
2024-11-22 01:14:41 +0100

    add readthedocs yaml configuration file

--- doc/conf.py
+++ doc/conf.py
@@ -216,10 +216,10 @@ def skip(app, what, name, obj, would_skip, options):
     return would_skip
 
 def setup(app):
-    app.add_css_file('css/custom.css')
-    app.add_css_file('css/pygments.css')
     app.connect("autodoc-skip-member", skip)
 
+html_css_files = ["css/custom.css",
+                  "css/pygments.css"]
 
 # -- Options for LaTeX output ---------------------------------------------
 
