0f654f3cea37aad4249e12dd33e8e602c178572a
C.A.P. Linssen <charl@turingbirds.com>
2024-12-02 11:13:11 -0800

    add readthedocs yaml configuration file

--- doc/conf.py
+++ doc/conf.py
@@ -194,7 +194,7 @@ html_logo = "https://raw.githubusercontent.com/nest/ode-toolbox/master/doc/fig/o
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['css', 'fig']
+html_static_path = ['css', 'fig', '_static']
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
