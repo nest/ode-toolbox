8c250a0f8c422d3886bd01ca36c737f4fee13a09
C.A.P. Linssen <charl@turingbirds.com>
2025-09-21 12:18:19 +0200

    sympy.Float copy() method workaround

--- odetoolbox/singularity_detection.py
+++ odetoolbox/singularity_detection.py
@@ -101,7 +101,11 @@ class SingularityDetection:
             a set with equations, where the left-hand side of each equation is the variable that is to be subsituted, and the right-hand side is the expression to put in its place
         """
         for val in sympy.flatten(A):
-            expr_sub = val.copy()
+            if isinstance(val, sympy.Float):
+                expr_sub = sympy.Float(float(val))    # work around https://github.com/sympy/sympy/issues/28951
+            else:
+                expr_sub = val.copy()
+
             for eq in cond_set:
                 expr_sub = expr_sub.subs(eq.lhs, eq.rhs)
 
